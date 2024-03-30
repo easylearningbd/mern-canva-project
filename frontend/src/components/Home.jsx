ac9f5a5bf3ebe7f26896d61df8eb097449351d96
easylearningbd <elebdsoft@gmail.com>
2024-03-31 01:19:20 +0600

    20 Home Page Create and Design Part 5

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            Home
+        <div className='pt-1 pl-3'>
+            <div className='w-full flex justify-center items-center h-[250px] bg-gradient-to-r from-[#4c76cf] to-[#552ab8] relative rounded-md overflow-hidden'>
+            
+            <button className='px-4 py-2 text-[15px] overflow-hidden text-center bg-[#32769ead] text-white rounded-[3px] font-medium hover:bg-[#1e830f] absolute top-3 right-3'>
+             Custom Size 
+            </button>
+
+                <div>
+                    <h2 className='text-3xl pb-10 pt-6 font-semibold text-white'>What Will You Design Today</h2>
+                </div>
+
+            </div>
         </div>
     );
 };
