fd54a42e776ff8a73960f3554ccf964085271caf
easylearningbd <elebdsoft@gmail.com>
2024-03-30 14:19:33 +0600

    17 Home Page Create and Design Part 2

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Layout = () => {
+    const [show,setShow] = useState(false)
+
     return (
 <div className='bg-[#18191b] min-h-screen w-full '>
     <div className='bg-[#212223] shadow-md fixed left-0 top-0 w-full z-20'>
@@ -13,10 +16,38 @@ const Layout = () => {
     <div className='flex gap-4 justify-center items-center relative'>
         <button className='py-2 px-2 overflow-hidden text-center bg-[#8b3dff] text-white rounded-[3px] font-medium'>Create a Design
         </button>
-        <div className=' cursor-pointer'>
-        <img className='w-[48px] h-[45px] rounded-full' src="https://templates-flatlogic.herokuapp.com/sing-app/html5/demo/img/people/a5.jpg" alt="" />
+        <div onClick={() => setShow(!show)} className=' cursor-pointer'>
+        <img className='w-[48px] h-[45px] rounded-full' src="https://templates-flatlogic.herokuapp.com/sing-app/html5/demo/img/people/a5.jpg" alt="" /> 
+        </div>
+        
+        <div className={`absolute top-[60px] right-0 w-[250px] bg-[#313030] p-3 border border-gray-700 transition duration-500 ${show ? 'visible opacity-100' : 'invisible opacity-30'} `}>
+       
+        <div className='px-2 py-2 flex justify-start gap-5 items-center'>
+        <img className='w-[40px] h-[40px] rounded-full' src="https://templates-flatlogic.herokuapp.com/sing-app/html5/demo/img/people/a5.jpg" alt="" /> 
+        <div className='flex justify-center flex-col items-start'>
+        <span className='text-[#e0dddd] font-bold text-md'>Ariyan</span>
+        <span className='text-[#e0dddd] font-bold text-md'>ariyan@gmail.com</span>
+           </div>
+         </div>
+
+        <ul className='text-[#e0dddd] font-semibold'>
+            <li>
+                <Link className='p-2 cursor-pointer'>
+                <span>Setting </span>
+                </Link>
+            </li>
+            <li>
+                <Link className='p-2 cursor-pointer'>
+                <span>Logout </span>
+                </Link>
+            </li>
+        </ul>
 
         </div>
+
+
+
+
      </div> 
 
        </div>
