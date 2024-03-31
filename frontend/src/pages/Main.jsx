01a0059de1e6bb9c08baf22251867bde66112a43
easylearningbd <elebdsoft@gmail.com>
2024-04-01 00:31:19 +0600

    32 Design Sidebar Pop Up Section Part 3

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -10,6 +10,8 @@ import { RxTransparencyGrid } from "react-icons/rx";
 import { MdKeyboardArrowLeft } from "react-icons/md";
 import TemplateDesign from '../components/main/TemplateDesign';
 import MyImages from '../components/MyImages';
+import Projects from '../components/Projects';
+import Image from '../components/Image';
 
 
 const Main = () => {
@@ -99,22 +101,32 @@ const Main = () => {
             }
             {
                 state === 'text' && <div>
-                    text
+                    <div className='grid grid-cols-1 gap-2'>
+                    <div className='bg-[#3c3c3d] cursor-pointer font-bold p-3 text-white text-xl rounded-sm'>
+                    <h2>Add A Text </h2>
+                    </div>
+                    </div>
                 </div>
             }
             {
-                state === 'project' && <div>
-                    project
+                state === 'project' &&  <Projects />
+            }
+            {
+                state === 'initImage' && <div className='h-[88vh] overflow-x-auto flex justify-start items-start scrollbar-hide'>
+                    <Image/>
                 </div>
             }
             {
-                state === 'initImage' && <div>
-                    initImage
-                </div>
-            }
+                state === 'background' && <div className='h-[88vh] overflow-x-auto flex justify-start items-start scrollbar-hide'>
+           <div className='grid grid-cols-2 gap-2'>
             {
-                state === 'background' && <div>
-                    background
+                [1,2,3,4,5,6].map((img, i) => <div key={i} className='w-full h-[90px] overflow-hidden rounded-sm cursor-pointer'>
+                <img className='w-full h-full object-fill' src="http://localhost:5173/canva.png" alt="" />
+                </div>
+                )
+            }
+            </div>
+                    
                 </div>
             }
 
