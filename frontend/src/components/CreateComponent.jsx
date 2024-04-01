3b0dc0f190f78134567018c3b18db6751b69a922
easylearningbd <elebdsoft@gmail.com>
2024-04-01 16:48:33 +0600

    41 Make Design Rotate Resize Move Indicator Part 1

--- frontend/src/components/CreateComponent.jsx
+++ frontend/src/components/CreateComponent.jsx
@@ -45,6 +45,63 @@ const CreateComponent = ({ info, current_component,removeComponent}) => {
     }
 
 
+    if (info.name === 'shape' && info.type === 'circle') {
+        html = <div id={randValue} onClick={() => info.setCurrentComponent(info)} style={{  
+            left: info.left + 'px',
+            top: info.top + 'px',
+            zIndex: info.z_index,
+            transform: info.rotate ? `rotate(${info.rotate}deg)` : 'rouate(0deg)' 
+         }} 
+         className='absolute group hover:border-[2px] hover:border-indigo-500' 
+         >
+            <div id={`${randValue}c`} className='rounded-full' style={{  
+            width: info.width + 'px',
+            height: info.width + 'px',
+            background: info.color,
+            opacity: info.opacity, 
+         }} >
+
+            </div>
+
+            {
+                current_component.id === info.id && <div onClick={() => removeComponent(info.id)} className='px-3 py-2 bg-white absolute top-0 hidden group-hover:block cursor-pointer rounded-md'>
+                    <FaTrashAlt />
+                </div>
+            }
+
+        </div>
+    }
+
+
+
+    if (info.name === 'shape' && info.type === 'trangle') {
+        html = <div id={randValue} onClick={() => info.setCurrentComponent(info)} style={{  
+            left: info.left + 'px',
+            top: info.top + 'px',
+            zIndex: info.z_index,
+            transform: info.rotate ? `rotate(${info.rotate}deg)` : 'rouate(0deg)' 
+         }} 
+         className='absolute group hover:border-[2px] hover:border-indigo-500' 
+         >
+            <div id={`${randValue}t`} style={{  
+            width: info.width + 'px',
+            height: info.height + 'px',
+            background: info.color,
+            opacity: info.opacity, 
+            clipPath: 'polygon(50% 0, 100% 100%, 0 100%)'
+         }} >
+
+            </div>
+
+            {
+                current_component.id === info.id && <div onClick={() => removeComponent(info.id)} className='px-3 py-2 bg-white absolute top-0 hidden group-hover:block cursor-pointer rounded-md'>
+                    <FaTrashAlt />
+                </div>
+            }
+
+        </div>
+    }
+
 
 
 
--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -177,7 +177,7 @@ const Main = () => {
                 state === 'shape' && <div className='grid grid-cols-3 gap-2'>
                     <div onClick={() => createShape('shape','rect')} className='h-[90px] bg-[#3c3c3d] cursor-pointer'></div>
                     <div onClick={() => createShape('shape','circle')} className='h-[90px] bg-[#3c3c3d] cursor-pointer rounded-full'></div>
-                    <div onClick={() => createShape('shape','trangle')} style={{clipPath: 'polygon(50% 0, 100% 100%, 0 100%'}} className='h-[90px] bg-[#3c3c3d] cursor-pointer  '></div>
+                    <div onClick={() => createShape('shape','trangle')} style={{clipPath: 'polygon(50% 0, 100% 100%, 0 100%)'}} className='h-[90px] bg-[#3c3c3d] cursor-pointer  '></div>
                     
                 </div>
             }
