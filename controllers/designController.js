ae2c76b0ddf411369dabdac3753efba1ec83b5d3
easylearningbd <elebdsoft@gmail.com>
2024-04-20 05:34:21 +0600

    72 Create Design And Store Database Part 6

--- controllers/designController.js
+++ controllers/designController.js
@@ -1,5 +1,6 @@
 const {formidable} = require('formidable')
 const cloudinary = require('cloudinary').v2
+const designModel = require('../models/designModel')
 
 class designController {
 
@@ -16,9 +17,15 @@ class designController {
             const { image } = files 
             const { url } = await cloudinary.uploader.upload(image[0].filepath)
 
-            
+            const design = await designModel.create({
+                user_id: _id,
+                components: [JSON.parse(fields.design[0])],
+                image_url: url
+            })
+            return res.status(200).json({ design }) 
+
         } catch (error) {
-            
+            return res.status(500).json({ message: error.message }) 
         }
         
     }
